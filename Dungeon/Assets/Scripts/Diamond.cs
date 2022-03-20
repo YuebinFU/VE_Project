7a7ef5e8183e7836fe0da7c376807872146ba177
JasDEng <1026198538@qq.com>
2022-03-21 06:31:55 +0800

    add network diamon

--- Dungeon/Assets/Scripts/Diamond.cs
+++ Dungeon/Assets/Scripts/Diamond.cs
@@ -1,36 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Ubiq.Messaging;
 using Ubiq.XR;
 using UnityEngine;
 
-public class Diamond : MonoBehaviour, IGraspable
-{
+public class Diamond : MonoBehaviour, IGraspable, INetworkComponent, INetworkObject {
     private Hand grasped;
 
-    void IGraspable.Grasp(Hand controller)
-    {
+    public NetworkId Id { get; set; }
+    // NetworkId INetworkObject.Id => new NetworkId(1001);
+
+    private NetworkContext context;
+
+    void IGraspable.Grasp (Hand controller) {
+        Debug.Log ("Clicked");
         grasped = controller;
+        Debug.Log ("grasped");
     }
 
-    void IGraspable.Release(Hand controller)
-    {
+    void INetworkComponent.ProcessMessage (ReferenceCountedSceneGraphMessage message) {
+        var msg = message.FromJson<Message> ();
+        transform.position = msg.position;
+    }
+
+    void IGraspable.Release (Hand controller) {
         grasped = null;
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
+        context = NetworkScene.Register (this);
+    }
 
+    struct Message {
+        public Vector3 position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-      if (grasped != null)
-      {
-          transform.position = grasped.transform.position;
-          transform.rotation = grasped.transform.rotation;
-      }
+    void Update () {
+        if (grasped != null) {
+            transform.position = grasped.transform.position;
+            transform.rotation = grasped.transform.rotation;
+            Message message;
+            message.position = transform.position;
+            context.SendJson (message);
+        }
     }
+
+    public void Awake () {
+        string newID = transform.position.ToString () + transform.rotation.ToString ();
+        // string newID = System.DateTime.Now.ToFileTime().ToString();
+        Id = new NetworkId ((uint) newID.GetHashCode ());
+        Debug.Log (Id);
+    }
+
 }
