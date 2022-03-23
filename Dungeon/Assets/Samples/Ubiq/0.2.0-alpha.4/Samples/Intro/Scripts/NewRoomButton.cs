89cefed913a31c4bdeccc925b04a9592b0b273c7
JasDEng <1026198538@qq.com>
2022-03-23 21:49:51 +0800

    change coordinate of the second player

--- Dungeon/Assets/Samples/Ubiq/0.2.0-alpha.4/Samples/Intro/Scripts/NewRoomButton.cs
+++ Dungeon/Assets/Samples/Ubiq/0.2.0-alpha.4/Samples/Intro/Scripts/NewRoomButton.cs
@@ -1,29 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Ubiq.Rooms;
 using UnityEngine;
 using UnityEngine.UI;
-using Ubiq.Rooms;
 
-namespace Ubiq.Samples
-{
-    public class NewRoomButton : MonoBehaviour
-    {
+namespace Ubiq.Samples {
+    public class NewRoomButton : MonoBehaviour {
         public SocialMenu mainMenu;
         public Text nameText;
         public bool publish;
 
         // Expected to be called by a UI element
-        public void NewRoom ()
-        {
-            mainMenu.roomClient.Join(
+        public void NewRoom () {
+            mainMenu.roomClient.Join (
                 name: nameText.text,
                 publish: publish);
-            
-            GameObject player = GameObject.Find("Player");
-            player.transform.position = new Vector3(1.208f, 91.5f, 35.82f);
-            player.GetComponent<StartDoorOpen>().playerType = 1;
-           // Debug.Log(StartDoorOpen.playerType);
-            //player.transform.position = new Vector3(1.253f, 103.0f, 10.5f);
+
+            GameObject player = GameObject.Find ("Player");
+            player.transform.position = new Vector3 (1.208f, 91.5f, 35.82f);
+            player.GetComponent<StartDoorOpen> ().playerType = 1;
+            // Debug.Log(StartDoorOpen.playerType);
+            // player.transform.position = new Vector3 (1.253f, 103.0f, 10.5f);
         }
     }
 }
