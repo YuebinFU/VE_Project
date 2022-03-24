4d9b72bd6f14b76b9a3bef28528371a0ba54ba54
JasDEng <1026198538@qq.com>
2022-03-25 00:33:35 +0800

    fix one of the door netwoking prob

--- Dungeon/Assets/Scripts/opendoor_up.cs
+++ Dungeon/Assets/Scripts/opendoor_up.cs
@@ -10,48 +10,71 @@ public class opendoor_up : MonoBehaviour, INetworkComponent, INetworkObject {
     public NetworkId Id { get; set; }
     // NetworkId INetworkObject.Id => new NetworkId(1001);
     private NetworkContext context;
+    public bool isTriggered;
+    public bool previousFlag;
 
     void INetworkComponent.ProcessMessage (ReferenceCountedSceneGraphMessage message) {
         var msg = message.FromJson<Message> ();
-        transform.position = msg.position;
-        transform.rotation = msg.rotation;
+        isTriggered = msg.isOpened;
+        previousFlag = isTriggered;
+        if (isTriggered) {
+            gameObject.GetComponent<AudioSource> ().Play ();
+            GameObject up_door = GameObject.Find ("Inner Environment/Doors/Trigger_Door_Up");
+            up_door.transform.localEulerAngles = new Vector3 (0.0f, 0.0f, 0.0f);
+            previousFlag = isTriggered;
+        } else {
+            gameObject.GetComponent<AudioSource> ().Play ();
+            GameObject up_door = GameObject.Find ("Inner Environment/Doors/Trigger_Door_Up");
+            up_door.transform.localEulerAngles = new Vector3 (0.0f, 116.45f, 0.0f);
+            previousFlag = isTriggered;
+        }
     }
 
     // Start is called before the first frame update
     void Start () {
         context = NetworkScene.Register (this);
-
     }
 
     struct Message {
-        public Vector3 position;
-        public Quaternion rotation;
+        public bool isOpened;
     }
 
     void OnCollisionEnter (Collision other) {
+        previousFlag = isTriggered;
+        isTriggered = true;
         gameObject.GetComponent<AudioSource> ().Play ();
         GameObject up_door = GameObject.Find ("Inner Environment/Doors/Trigger_Door_Up");
         up_door.transform.localEulerAngles = new Vector3 (0.0f, 0.0f, 0.0f);
     }
+
     void OnCollisionExit (Collision other) {
+        previousFlag = isTriggered;
+        isTriggered = false;
         gameObject.GetComponent<AudioSource> ().Play ();
         GameObject up_door = GameObject.Find ("Inner Environment/Doors/Trigger_Door_Up");
         up_door.transform.localEulerAngles = new Vector3 (0.0f, 116.45f, 0.0f);
     }
+
     // Update is called once per frame
     void Update () {
-
-        Message message;
-        message.position = transform.position;
-        message.rotation = transform.rotation;
-        context.SendJson (message);
-
+        if (isTriggered != previousFlag) {
+            // Debug.Log ("Flag: ");
+            // Debug.Log (isTriggered);
+            // Debug.Log ("previous flag: ");
+            // Debug.Log (previousFlag);
+            previousFlag = isTriggered;
+            Message message;
+            message.isOpened = isTriggered;
+            context.SendJson (message);
+        }
     }
 
     public void Awake () {
         string newID = transform.position.ToString () + transform.rotation.ToString ();
         // string newID = System.DateTime.Now.ToFileTime().ToString();
         Id = new NetworkId ((uint) newID.GetHashCode ());
+        isTriggered = false;
+        previousFlag = isTriggered;
         Debug.Log (Id);
     }
 }
